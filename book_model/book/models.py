bb1dbbcb76be6a9795db79a1dc58d4cb15927dfa
Om Pratap Singh <ompratap1999@gmail.com>
2024-09-15 21:58:21 +0530

    Initial commit

---
+++ book_model/book/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class author(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=100)
